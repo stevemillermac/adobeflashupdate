2904a3dae491ff0da886ee3c13b0557a26b2cdd2
Steve <stevemillermac@gmail.com>
2017-02-17 09:00:27 -0500

    Update adobeflashupdate.sh

--- adobeflashupdate.sh
+++ adobeflashupdate.sh
@@ -13,21 +13,68 @@
 #
 # HISTORY
 #
-#   Version: 1.2
+#   Version: 1.5
 #
-#   - v.1.0	Steve Miller, 16.12.2015:	Used Joe Farage "AdobeReaderUpdate.sh as starting point
-#   - v.1.1	Steve Miller, 16.12.2015:	Updated to copy echo commands into JSS policy logs
-#   - v.1.2	Steve Miller, 21.12.2015:	Updated umount command to use hdiutil. 10.9 issues previous command
+#   - v.1.0 Steve Miller, 16.12.2015:   Used Joe Farage "AdobeReaderUpdate.sh as starting point
+#   - v.1.1 Steve Miller, 16.12.2015:   Updated to copy echo commands into JSS policy logs
+#   - v.1.2 Steve Miller, 21.12.2015:   Updated umount command to use hdiutil. 10.9 issues previous command
+#   - v.1.3 Steve Miller, 13.05.2016:   Updated to fix line 45 for change on website.
+#   - v.1.4 Steve Miller, 16.12.2016:   Reworked to utilize code from Luis Lugo in Reader script.
+#   - v.1.5 Steve Miller, 11.01.2017:   Change to Adobe download URL format
 #
 ####################################################################################################
 # Script to download and install Adobe Flash.
 # Only works on Intel systems.
 
+# Setting variables
 dmgfile="flash.dmg"
-dmgmount="/Volume/Flash Player"
-mntpoint=`diskutil list | grep Flash | awk '{print $7}' `
-volname="Flash"
-logfile="/Library/Logs/FlashUpdateScript.log"
+
+# Echo function
+echoFunc () {
+    # Date and Time function for the log file
+    fDateTime () { echo $(date +"%a %b %d %T"); }
+
+    # Title for beginning of line in log file
+    Title="InstallLatestAdobeFlash:"
+
+    # Header string function
+    fHeader () { echo $(fDateTime) $(hostname) $Title; }
+
+    # Check for the log file
+    if [ -e "/Library/Logs/AdobeFlashUpdateScript.log" ]; then
+        echo $(fHeader) "$1" >> "/Library/Logs/AdobeFlashUpdateScript.log"
+    else
+        cat > "/Library/Logs/AdobeFlashUpdateScript.log"
+        if [ -e "/Library/Logs/AdobeFlashUpdateScript.log" ]; then
+            echo $(fHeader) "$1" >> "/Library/Logs/AdobeFlashUpdateScript.log"
+        else
+            echo "Failed to create log file, writing to JAMF log"
+            echo $(fHeader) "$1" >> "/var/log/jamf.log"
+        fi
+    fi
+
+    # Echo out
+    echo $(fDateTime) ": $1"
+}
+
+# Exit function
+# Exit code examples: http://www.tldp.org/LDP/abs/html/exitcodes.html
+exitFunc () {
+    case $1 in
+        0) exitCode="0 - SUCCESS: Adobe Flash up to date with version $2";;
+        3) exitCode="3 - INFO: Adobe Flash NOT installed!";;
+        4) exitCode="4 - ERROR: Adobe Flash update unsuccessful, version remains at $2";;
+        6) exitCode="6 - ERROR: Not an Intel-based Mac.";;
+        *) exitCode="$1";;
+    esac
+    echoFunc "Exit code: $exitCode"
+    echoFunc "======================== Script Complete ========================"
+    exit $1
+}
+
+echoFunc ""
+echoFunc "======================== Starting Script ========================"
+
 
 # Are we running on Intel?
 if [ '`/usr/bin/uname -p`'="i386" -o '`/usr/bin/uname -p`'="x86_64" ]; then
@@ -41,76 +88,86 @@ if [ '`/usr/bin/uname -p`'="i386" -o '`/usr/bin/uname -p`'="x86_64" ]; then
     latestver=``
     while [ -z "$latestver" ]
     do
-        latestver=`/usr/bin/curl -s http://www.adobe.com/software/flash/about/ | sed -n '/Safari/,/<\/tr/s/[^>]*>\([0-9].*\)<.*/\1/p'`
-    done
-
-    echo "Latest Version is: $latestver"
-    latestvernorm=`echo $ {latestver} `
-    # Get the version number of the currently installed Adobe Flash, if any.
+    	latestver=`/usr/bin/curl -s http://www.adobe.com/software/flash/about/ | sed -n '/Firefox, Safari - NPAPI/,/<\/tr/s/[^>]*>\([0-9].*\)<.*/\1/p'`
+	done
+	
+	echoFunc "Latest Adobe Flash Version is: ${latestver}"
+	latestvernorm=`echo $ {latestver} `
+    # Get the version number of the currently-installed Adobe Flash, if any.
     if [ -e "/Library/Internet Plug-Ins/Flash Player.plugin/Contents/" ]; then
-        currentinstalledver=`/usr/bin/defaults read "/Library/Internet Plug-Ins/Flash Player.plugin/Contents/Info" CFBundleShortVersionString`
-        echo "Currently installed version is: $currentinstalledver"
-        if [ "${latestver}" = "${currentinstalledver}" ]; then
-            echo "Adobe Flash is current. Exiting"
-            exit 0
-        fi
+		currentinstalledapp="Adobe Flash"
+		currentinstalledver=`/usr/bin/defaults read "/Library/Internet Plug-Ins/Flash Player.plugin/Contents/Info" CFBundleShortVersionString`
+    	echoFunc "Currently installed version is: ${currentinstalledver}"
+    	if [ "${latestvernorm}" = "${currentinstalledver}" ]; then
+    		exitFunc 0 "${currentinstalledapp} ${currentinstalledver}"
+    	fi
     else
-       currentinstalledver="none"
-       echo "Adobe Flash is not installed"
+        currentinstalledapp="Adobe Flash"
+        echoFunc "${currentinstalledapp} is not installed"
     fi
-
-
-    shortver=${latestver:0:2}
-    echo "Flash Short version: $shortver"
-    url1="http://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_${shortver}_osx.dmg"
-
-    #Build URL  
-    url=`echo "${url1}"`
-    echo "Latest version of the URL is: $url"
     
-
+	#Build URL
+	shortver=${latestver:0:2}
+    url1="https://fpdownload.adobe.com/get/flashplayer/pdc/"${latestver}"/install_flash_player_osx.dmg"
+    url2=""
+    url=`echo "${url1}${url2}"`
+    
+    
     # Compare the two versions, if they are different of Flash is not present then download and install the new version.
     if [ "${currentinstalledver}" != "${latestver}" ]; then
-        /bin/echo "`date`: Current Flash version: ${currentinstalledver}" >> ${logfile}
-        /bin/echo "`date`: Available Flash version: ${latestver}" >> ${logfile}
-        /bin/echo "`date`: Downloading newer version." >> ${logfile}
-        /usr/bin/curl -s -o /tmp/${dmgfile} ${url}
-        /bin/echo "`date`: Mounting installer disk image." >> ${logfile}
-        /usr/bin/hdiutil attach /tmp/${dmgfile} -nobrowse -quiet
-        /bin/echo "`date`: Installing..." >> ${logfile}
-        /usr/sbin/installer -pkg /Volumes/Flash\ Player/Install\ Adobe\ Flash\ Player.app/Contents/Resources/Adobe\ Flash\ Player.pkg -target / > /dev/null
+        echoFunc "Versions differ as following"
+        echoFunc "Latest Adobe Flash Version is: ${latestver}"
+        echoFunc "Currently installed version is: ${currentinstalledver}"
+        echoFunc "Latest version of the URL is: $url"
+        echoFunc "Downloading newer version."
+        curl -s -o /tmp/${dmgfile} ${url}
+        case $? in
+            0)
+                echoFunc "Checking if the file exists after downloading."
+                if [ -e "/tmp/${dmgfile}" ]; then
+                    readerFileSize=$(du -k "/tmp/${dmgfile}" | cut -f 1)
+                    echoFunc "Downloaded File Size: $readerFileSize kb"
+                else
+                    echoFunc "File NOT downloaded!"
+                    exitFunc 3 "${currentinstalledapp} ${currentinstalledver}"
+                fi
+                echoFunc "Mounting installer disk image."
+                hdiutil attach /tmp/${dmgfile} -nobrowse -quiet
+                echoFunc "Installing..."
+        		installer -pkg /Volumes/Flash\ Player/Install\ Adobe\ Flash\ Player.app/Contents/Resources/Adobe\ Flash\ Player.pkg -target / > /dev/null
         
-        #Unmount DMG and deleting tmp files
-        /bin/sleep 10
-        /bin/echo "`date`: Unmounting installer disk image." >> ${logfile}
-        mntpoint=`diskutil list | grep Flash | awk '{print $7}' `
-        /bin/echo The mount point is "$mntpoint"
-        hdiutil unmount $mntpoint -force -quiet
-        hdiutil detach $mntpoint -force -quiet
-        /bin/sleep 10
-        /bin/echo "`date`: Deleting disk image." >> ${logfile}
-        /bin/rm /tmp/${dmgfile}
+        		#Unmount DMG and deleting tmp files
+				sleep 10
+				mntpoint=`diskutil list | grep Flash | awk '{print $7}' `
+                #echoFunc "The mount point is ${mntpoint}"
+				echoFunc "Unmounting installer disk image."
+                umount "/Volumes/Flash Player"
+                #hdiutil detach -force -quiet "${mntpoint}"
+                sleep 10
+                echoFunc "Deleting disk image."
+                rm /tmp/${dmgfile}
 
         #Double check to see if the new version got updated
-        newlyinstalledver=`/usr/bin/defaults read "/Library/Internet Plug-Ins/Flash Player.plugin/Contents/version" CFBundleShortVersionString`
-        if [ "${latestver}" = "${newlyinstalledver}" ]; then
-            /bin/echo "SUCCESS: Flash has been updated to version ${newlyinstalledver}"
-            /bin/echo "`date`: SUCCESS: Flash has been updated to version ${newlyinstalledver}" >> ${logfile}
-        else
-            /bin/echo "ERROR: Flash update unsuccessful, version remains at ${currentinstalledver}."
-            /bin/echo "`date`: ERROR: Flash update unsuccessful, version remains at ${currentinstalledver}." >> ${logfile}
-            /bin/echo "--" >> ${logfile}
-            exit 1
+        if [ -e "/Library/Internet Plug-Ins/Flash Player.plugin" ]; then
+	        newlyinstalledver=`/usr/bin/defaults read "/Library/Internet Plug-Ins/Flash Player.plugin/Contents/Info" CFBundleShortVersionString`
+			if [ "${latestver}" = "${newlyinstalledver}" ]; then
+            	echoFunc "SUCCESS: Flash has been updated to version ${newlyinstalledver}, issuing JAMF recon command"
+            	jamf recon
+			fi
+            exitFunc 0 "${newlyinstalledver}"
         fi
-        
-    # If Flash is up to date already, just log it and exit.       
-    else
-        /bin/echo "Flash is already up to date, running ${currentinstalledver}."
-        /bin/echo "`date`: Flash is already up to date, running ${currentinstalledver}." >> ${logfile}
-        /bin/echo "--" >> ${logfile}
+    ;;
+	*)
+		echoFunc "Curl function failed on alternate download! Error: $?. Review error codes here: https://curl.haxx.se/libcurl/c/libcurl-errors.html"
+		exitFunc 4 "${currentinstalledapp} ${currentinstalledver}"
+	;;
+	esac
+else
+           
+        # If Flash is up to date already, just log it and exit.
+        exitFunc 0 "${currentinstalledapp} ${currentinstalledver}"
     fi
 else
-	/bin/echo "`date`: ERROR: This script is for Intel Macs only." >> ${logfile}
+    # This script is for Intel Macs only.
+    exitFunc 6
 fi
-
-exit 0
